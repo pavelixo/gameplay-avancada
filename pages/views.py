400f8246b4311e442c8565effcb5faf5ff990535
pavelixo <trashxindoragon87@gmail.com>
2024-06-16 14:41:50 -0300

    ...

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,15 @@
-from .mixins import CacheMixin
+from datetime import timedelta as time
 from discord.views import DiscordView
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 
-class Home(CacheMixin, DiscordView):
+class Home(DiscordView):
   template_name = 'home.html'
+
+  @method_decorator(cache_page(time(hours=16)))
+  def dispatch(self, *args, **kwargs):
+      return super().dispatch(*args, **kwargs)
 
   def get(self, request):
     context = {
