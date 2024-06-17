b17764f083d826124a3cb9f3b54b944508fdef9c
pavelixo <trashxindoragon87@gmail.com>
2024-06-17 19:17:13 -0300

    ...

--- pages/templatetags/validators.py
+++ pages/templatetags/validators.py
@@ -5,6 +5,6 @@ register = template.Library()
 
 @register.filter(name='validate_content_length')
 def validate_content_length(value: Any) -> str:
-    if len(value) > 16:
-        return f'{value[:13]}...'
+    if len(value) > 32:
+        return f'{value[:29]}...'
     return value
