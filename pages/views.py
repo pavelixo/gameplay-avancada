2f1389ec370029f4878ac602f2c6eb306c661a0c
pavelixo <trashxindoragon87@gmail.com>
2024-10-06 16:06:20 -0300

    cache

--- pages/views.py
+++ pages/views.py
@@ -11,8 +11,8 @@ class Home(DiscordView):
   template_name = 'home.html'
 
 
-  # @method_decorator(cache_page(60 * 60))
-  # @method_decorator(vary_on_cookie)
+  @method_decorator(cache_page(60 * 60))
+  @method_decorator(vary_on_cookie)
   def get(self, request):
     users = self.member_service.get_members()
     text_channels = self.guild_service.get_channels()
